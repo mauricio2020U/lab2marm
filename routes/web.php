ade0d1320f212c44710bc81068c3449f3edf73ed
mauricio2020U <mauricio.ramirez@catolica.edu.sv>
2022-04-23 20:04:57 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::get('productos','App\Http\Controllers\HomeController@indexprod')->name('productos');
+
+Route::get('/productos',[App\Http\Controllers\ProductosController::class, 'index'])->name('productos');
+Route::get('/create',[App\Http\Controllers\ProductosController::class, 'create'])->name('create');
+Route::post('/create',[App\Http\Controllers\ProductosController::class, 'store'])->name('store');
+Route::delete('/productos/{id}', [App\Http\Controllers\ProductosController::class,'destroy']);
+Route::get('/productos/{id}/edit', [App\Http\Controllers\ProductosController::class,'edit']);
+Route::put('/productos/{id}', [App\Http\Controllers\ProductosController::class,'update']);
